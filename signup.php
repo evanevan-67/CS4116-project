c5d4299923b006f7be90e53c22066f335f6b51b7
eamonngorman <79876756+eamonngorman@users.noreply.github.com>
2024-03-19 15:43:10 +0000

    Altered code to account for second password variable

--- signup.php
+++ signup.php
@@ -2,8 +2,8 @@
 
 $servername = "intrysts.free.nf";
 $database = "if0_36142329_intrysts";
-$username = "if0_36142329";
-$password = "eamonnmatt";
+$dbusername = "if0_36142329";
+$dbpassword = "eamonnmatt";
 
 $name = $_POST["name"];
 $email = $_POST["email"];
@@ -15,7 +15,7 @@ $occupation = $_POST["occupation"];
 $location = $_POST["location"];
 
 //Create Connection
-$conn = mysqli_connect($servername, $username, $password, $database);
+$conn = mysqli_connect($servername, $dbusername, $dbpassword, $database);
 // Check the connection
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
@@ -24,8 +24,8 @@ echo "Connected successfully";
 $sql = "INSERT INTO users (name, email, password) VALUES ($name, $email, $password)";
 if (mysqli_query($conn, $sql)) {
      echo "New user record created successfully";
-     $userid = mysqli_insert_id($conn)
-     $sql2 = "INSERT INTO profiles (userid, name, dob, gender, interestedin, occupation, location) VALUES ($userid, $name, $dob, $gender, $interestedin, $occupation, $location)";
+     $userid = mysqli_insert_id($conn);
+     $sql2 = "INSERT INTO profiles (userid, name, dob, gender, interestedin, occupation, location) VALUES ('$userid', '$name', '$dob', '$gender', '$interestedin', '$occupation', '$location')";
      if (mysqli_query($conn, $sql2)) {
         echo "New profile record created successfully";
    } else {
