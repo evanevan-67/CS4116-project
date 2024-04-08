0f814783238ee82f1976130015cd0f02656e0ac7
eamonngorman <79876756+eamonngorman@users.noreply.github.com>
2024-04-08 11:51:20 +0100

    Connection.php initial attempt at coding

--- connection.php
+++ connection.php
@@ -7,9 +7,94 @@ $profileid = $_GET['profileid'];
 $connection =  $_GET['connection'];
 
 // Check database for connectionid where user1id = $userid || $profileid AND user2id = $userid || $profileid
+
+$query = "SELECT connectionid, CASE WHEN user1id = $userid THEN 'user1id'
+                                    WHEN user2id = $userid THEN 'user2id'
+                                    ELSE NULL
+                                    END AS matched_column 
+                                    FROM connections WHERE (user1connection = $userid OR user1connection = $profileid) 
+                                                    AND (user2connection = $userid OR user2connection = $profileid);";
+$result = mysqli_query($conn, $query);
+
+
 // If none, create new connection user1id = $userid, user2id = $profileid, user1connection = $connection
+if(mysqli_num_rows($result) == 0) {
+    $stmt = $conn->prepare("INSERT INTO `connections` (`user1id`, `user2id`, `user1connection`) VALUES (?, ?, ?)");
+    $stmt->bind_param('iii', $userid, $profileid, $connection);
+
+    if ($stmt->execute()) {
+        echo "New connection created successfully";
+        $connectionid = $stmt->insert_id;
+    } else {
+        echo "Error: " . $stmt->error;
+   }
+   $stmt->close();
+}
+
+
 // If one exists, find out whether user1id or user2id matches $userid, then change the relevant user1/2connection to be = connection
+if(mysqli_num_rows($result) == 1) {
+    $row = mysqli_fetch_assoc($result);
+    if ($row['matched_column'] == user1id){
+        $stmt2 = $conn->prepare("UPDATE connections SET user1connection = $connection WHERE connectionid = $row['connectionid'];");
+        if ($stmt2->execute()) {
+            echo "Connection updated successfully";
+        } else {
+            echo "Error: " . $stmt2->error;
+       }
+    } else {
+        $stmt2 = $conn->prepare("UPDATE connections SET user2connection = $connection WHERE connectionid = $row['connectionid'];");
+        if ($stmt2->execute()) {
+            echo "Connection updated successfully";
+        } else {
+            echo "Error: " . $stmt2->error;
+       }
+    }
+    
+}
 // if user1connection and user2connection are both true at the end of the sequence, change connectionstatus to true
+
+$row = mysqli_fetch_assoc($result);
+
+$query2 = "SELECT user1connection, user2connection FROM connections WHERE connectionid = $row['connectionid'];";
+$result2 = mysqli_query($conn, $query2);
+
+$row2 = mysqli_fetch_assoc($result2);
+
+if ($row2['user1connection'] && $row2['user2connection']){
+    $stmt3 = $conn->prepare("UPDATE connections SET connectionstatus = 1 WHERE connectionid = $row['connectionid'];");
+        if ($stmt3->execute()) {
+            echo "Connection status updated successfully to true";
+        } else {
+            echo "Error: " . $stmt3->error;
+       }
+
+    $query3 = "SELECT chatid FROM chats WHERE connectionid = $row['connectionid'];";
+    $result3 = mysqli_query($conn, $query3);
+
+    if(mysqli_num_rows($result3) == 0) {
+        $stmt4 = $conn->prepare("INSERT INTO `chats` (`connectionid`, `user1id`, `user2id`) VALUES (?, ?, ?)");
+        $stmt4->bind_param('iii', $row['connectionid'], $userid, $profileid);
+
+        if ($stmt4->execute()) {
+            echo "New chat created successfully";
+            
+        } else {
+        echo "Error: " . $stmt4->error;
+        }
+        $stmt4->close();
+    }
+} else {
+    $stmt5 = $conn->prepare("UPDATE connections SET connectionstatus = 0 WHERE connectionid = $row['connectionid'];");
+        if ($stmt5->execute()) {
+            echo "Connection status updated successfully to false";
+        } else {
+            echo "Error: " . $stmt5->error;
+       }
+}
+
+
+
 // if no chat exists with this connectionstatus(and it's true), create a chat
 // if either are false, change connectionstatus to false
 
