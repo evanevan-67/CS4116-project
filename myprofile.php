96cc4dff15931d8882e341d77bb5edf3c2d95e42
eamonngorman <79876756+eamonngorman@users.noreply.github.com>
2024-04-10 20:20:33 +0100

    Tried to add photo uploads

--- myprofile.php
+++ myprofile.php
@@ -35,7 +35,11 @@
     <div class="profilephoto">
         <img src="Images/ProfilePlaceholder.PNG">
     </div>
-
+    
+    <form action="upload_image.php" method="POST" enctype="multipart/form-data">
+        <input type="file" name = "file">
+        <button type="submit" name="submit">Upload</button>
+    </form>
 
 
     <?php
