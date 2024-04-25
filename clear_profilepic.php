74236c8ce152da39bdebbf019296f5baad103649
eamonngorman <79876756+eamonngorman@users.noreply.github.com>
2024-04-25 14:35:20 +0100

    Fixed issue with clear_profilepic.php script

--- clear_profilepic.php
+++ clear_profilepic.php
@@ -1,13 +1,12 @@
 <?php
-include 'db_connect.php'
+include 'db_connect.php';
 
-    $profileid = $_GET('profileid');
-    // Update profilepic column to be blank
-    $sql = "UPDATE profiles SET profilepic = '' WHERE userid = ?";
-    $stmt = $pdo->prepare($sql);
-    $stmt->execute([$profileid]);
-    
-    header("Location: adminprofileedit.php?profileid=$profileid");
+$profileid = $_GET['profileid'];
 
- 
+// Update profilepic column to be blank
+$sql = "UPDATE profiles SET profilepic = '' WHERE userid = '{$profileid}'";
+mysqli_query($conn, $sql); // Execute the query using mysqli
+
+header("Location: adminprofileedit.php?profileid=$profileid");
+exit(); // Ensure that no further code is executed after redirection
 ?>
