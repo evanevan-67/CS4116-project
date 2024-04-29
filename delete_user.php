51b04136c94a15f6152d32dbafbb42040e36c73d
eamonngorman <79876756+eamonngorman@users.noreply.github.com>
2024-04-29 10:26:26 +0100

    Implemented admin functions

--- delete_user.php
+++ delete_user.php
@@ -4,9 +4,13 @@ include 'db_connect.php';
 $profileid = $_GET['profileid'];
 
 // Delete user and profile
-$sql = "DELETE FROM `users` WHERE userid = '{$profileid}'; DELETE FROM `profiles` WHERE userid = '{$profileid}';"
+$sql = "DELETE FROM `users` WHERE userid = '{$profileid}'";
+$sql2 = "DELETE FROM `profiles` WHERE userid = '{$profileid}'";
 mysqli_query($conn, $sql); // Execute the query using mysqli
+mysqli_query($conn, $sql2); // Execute the query using mysqli
 
+
+echo "Account deleted";
 // header("Location: admindashboard.php");
 header('Refresh: 10; URL=admindashboard.php');
 exit(); // Ensure that no further code is executed after redirection
